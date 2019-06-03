53994bae720480bff1ba13403ad93dcc401c653b
Elivelton Rodrigues <elivelton.rodrigues@ufrpe.br>
2019-06-03 17:17:43 -0300

    Model Viagem
    Criação do objeto viagem

--- voacampeao/viagem/models.py
+++ voacampeao/viagem/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Viagem(models.Model):
+    STATUS_CHOICES=(('1','EM AVALIAÇÃO'),
+                    ('2','ABERTO'),
+                    ('3','EM PATROCINIO'),
+                    ('4', 'PATROCINADA'))
+    origem = models.CharField(max_length=30, null=False, blank=False)
+    destino = models.CharField(max_length=30, null=False, blank=False)
+    data_ida = models.DateField(auto_now= False, auto_now_add = False)
+    data_volta = models.DateField(auto_now= False, auto_now_add = False)
+    descricao_comp = models.TextField(null=False, blank=False)
+    modalidade_comp = models.CharField(max_length=30, null=False)
+    path_documento = models.FilePathField(null=False, blank=False)
+    status = models.IntegerField(null=False, blank=False, choices='STATUS_CHOICES')
+    #id_atleta
+    #id_patrocinador
+
+
