046abe64ed3f7ef50f392de395a0fa7bb73d4eca
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2019-06-04 20:07:01 -0300

    Pequena atualização no form
    atualização no form e models de usuário.

--- voacampeao/usuario/models.py
+++ voacampeao/usuario/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+import re
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
 
 class Usuario(models.Model):
     SEXO_CHOICES = (
@@ -8,7 +12,8 @@ class Usuario(models.Model):
         ("F","Feminino"),
         ("N","Não informar")
     )
-    cpf = models.CharField(primary_key=True,max_length=11,null=False,blank=False)
+    cpf = models.CharField(primary_key=True,max_length=11,null=False,blank=False,
+                           validators=[MinLengthValidator(11)])
     nome = models.CharField(max_length=100, null=False,blank=False)
     sexo = models.CharField(max_length=1, null=False, blank=False, choices=SEXO_CHOICES)
     data_nascimento = models.DateField(null=False, blank=False)
