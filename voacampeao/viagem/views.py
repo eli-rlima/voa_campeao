2717f003f8af58ff7b1b850e96a01a752aa3403f
Elivelton Rodrigues <elivelton.rodrigues@ufrpe.br>
2019-06-05 18:16:13 -0300

    Feature|Funcionalidade de busca close #5
    Filtro por origem, destino e modalidade.

--- voacampeao/viagem/views.py
+++ voacampeao/viagem/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from .models import Viagem
 from .serializers import ViagemSerializer
+from rest_framework import filters
 
 class Viagens(viewsets.ModelViewSet):
     queryset = Viagem.objects.all()
     serializer_class = ViagemSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('origem', 'destino')
+
