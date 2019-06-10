be7992b9708b1a8e335ca2d40f28fef2f3193faa
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2019-06-10 07:04:42 -0300

    Feature | Issue #8 e #9
    Adicionado métodos na view de viagem para retornar viagens por status utilizando get;
    Adicionado também para retornar viagens do atleta, passando o atleta como parâmetro.

--- voacampeao/viagem/views.py
+++ voacampeao/viagem/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework import filters
 from .models import Viagem, Patrocinio
 from .serializers import ViagemSerializer, PatrocinioSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class Viagens(viewsets.ModelViewSet):
     '''cria view via rest com filtros para busca.'''
@@ -10,6 +12,20 @@ class Viagens(viewsets.ModelViewSet):
     serializer_class = ViagemSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('origem', 'destino', 'modalidade_comp')
+
+    @action(methods=['get'],detail=False)
+    def viagemPorUsuario(self, request, pk=None):
+        cpf = self.request.query_params.get('atleta',None)
+        queryset = Viagem.objects.filter(atleta=cpf)
+        viagens = ViagemSerializer(queryset, many=True, context={'request': request})
+        return Response(viagens.data)
+
+    @action(methods=['get'], detail=False)
+    def viagenStatus(self, request, pk=None):
+        status = self.request.query_params.get('status', None)
+        queryset = Viagem.objects.filter(status=status)
+        viagens = ViagemSerializer(queryset, many=True, context={'request': request})
+        return Response(viagens.data)
 
 class Patrocinios(viewsets.ModelViewSet):
     queryset = Patrocinio.objects.all()
