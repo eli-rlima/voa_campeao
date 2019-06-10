b68f3e5d8398824479fe74c26d2d8f1703b34005
EliveltonR18 <elivelton.rodrigues@ufrpe.br>
2019-06-10 17:19:13 -0300

    Feature| Filtro por proximidade

--- voacampeao/viagem/views.py
+++ voacampeao/viagem/views.py
@@ -1,10 +1,12 @@
 '''Classe que cria views para acesso a viagens no banco.'''
 from rest_framework import viewsets
 from rest_framework import filters
-from .models import Viagem, Patrocinio
-from .serializers import ViagemSerializer, PatrocinioSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from .models import Viagem, Patrocinio
+from .serializers import ViagemSerializer, PatrocinioSerializer
+from datetime import datetime
+from datetime import timedelta
 
 class Viagens(viewsets.ModelViewSet):
     '''cria view via rest com filtros para busca.'''
@@ -26,6 +28,17 @@ class Viagens(viewsets.ModelViewSet):
         queryset = Viagem.objects.filter(status=status)
         viagens = ViagemSerializer(queryset, many=True, context={'request': request})
         return Response(viagens.data)
+    
+    @action(methods=['get'], detail=False)
+    def viagemProximidade(self, request, pk=None):
+        dias = self.request.query_params.get('dias', None)
+        dateProx = datetime.today() + timedelta(days=int (dias))
+        queryset = Viagem.objects.exclude(data_ida__gt=dateProx)
+        viagens = ViagemSerializer(queryset, many=True, context={'request': request})
+        return Response(viagens.data)
+        
+
+        
 
 class Patrocinios(viewsets.ModelViewSet):
     queryset = Patrocinio.objects.all()
